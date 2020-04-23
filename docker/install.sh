16b0beddba4ce2027df8aefd4b7608dd5078ff3b
avalitan <sgtflux@gmail.com>
2020-04-23 19:37:41 +0200

    Create install.sh

---
+++ docker/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERSION=6.1
+echo "deb http://packages.zentyal.org/zentyal $VERSION main" > /etc/apt/sources.list.d/zentyal-archive.list
+wget -q http://keys.zentyal.org/zentyal-$VERSION-packages.asc -O /etc/apt/trusted.gpg.d/zentyal-packages.asc
+
+apt update
+
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends zentyal && echo "
+You can access the Zentyal Web Interface at:
+ * https://<zentyal-ip-address>:8443/
+"
